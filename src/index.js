a57cbfd5c1d9473d930a23d2441d50049a458c55
Maksim Astravukh <astravukh@gmail.com>
2018-09-27 11:34:19 +0300

    ref solution 10 del period

--- src/index.js
+++ src/index.js
@@ -2,30 +2,23 @@ module.exports = function count(s, pairs) {
   var N = 1;
   for(var i = 0; i < pairs.length; i++){
     N *= pairs[i][0] ** pairs[i][1];
-  }  
-  var period = 1;
-  for(let i =0; i < pairs.length; i++){
-    period *= pairs[i][0];
   }
-  var countByPeriod = 0;
 
   if (N > 100000000) return false;
 
-  for (let i = 0; i < period; i++){
+  let count = 0;
+
+  for (let i = 0; i < N; i++){
     for (let j = 0; j < s.length; j++) {
       if (getOneOrZero(i + j, pairs) != s[j]){        
         break;
       }
       if(j == s.length - 1){
-        countByPeriod++;
+        count++;
       }
     } 
   }
-  
-  var periodsCount = N/period;
-  
-  var result = periodsCount * countByPeriod;
-  //var result = count% 1000000007;
+  var result = count % 1000000007;
   return result;
 }
 function getOneOrZero(i, pairs) {
